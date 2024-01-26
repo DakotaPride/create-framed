184dea897d9b0318807cc4fd49a535ce559e957b
DakotaPride <rjtube13@gmail.com>
2024-01-26 11:12:48 -0600

    Fix Framed Glass Doors on Trains

--- src/main/java/net/dakotapride/createframed/block/door/TintedFramedGlassSlidingDoorBlock.java
+++ src/main/java/net/dakotapride/createframed/block/door/TintedFramedGlassSlidingDoorBlock.java
@@ -1,16 +1,7 @@
 package net.dakotapride.createframed.block.door;
 
-import com.simibubi.create.content.decoration.slidingDoor.SlidingDoorBlock;
-import net.dakotapride.createframed.registry.CreateFramedEntityTypes;
-import net.minecraft.world.level.block.entity.BlockEntityType;
-
-public class TintedFramedGlassSlidingDoorBlock extends SlidingDoorBlock {
+public class TintedFramedGlassSlidingDoorBlock extends FramedGlassSlidingDoorBlock {
     public TintedFramedGlassSlidingDoorBlock(Properties properties) {
-        super(properties, false);
-    }
-
-    @Override
-    public BlockEntityType<? extends FramedGlassSlidingDoorBlockEntity> getBlockEntityType() {
-        return CreateFramedEntityTypes.SLIDING_DOOR.get();
+        super(properties);
     }
 }
