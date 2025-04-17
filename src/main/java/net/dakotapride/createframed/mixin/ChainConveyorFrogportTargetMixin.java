ae33d03d56882c259b2d40c037fc7f31866a0ab3
Dakota <rjtube13@gmail.com>
2025-04-17 09:33:28 -0500

    1.6.4

--- src/main/java/net/dakotapride/createframed/mixin/ChainConveyorFrogportTargetMixin.java
+++ src/main/java/net/dakotapride/createframed/mixin/ChainConveyorFrogportTargetMixin.java
@@ -1,7 +1,7 @@
 package net.dakotapride.createframed.mixin;
 
 import com.simibubi.create.content.logistics.packagePort.PackagePortTarget;
-import net.dakotapride.createframed.registry.CreateFramedEntityTypes;
+import net.dakotapride.createframed.registry.CreateFramedFrogports;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,17 +12,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class ChainConveyorFrogportTargetMixin {
     @Inject(method = "canSupport", at = @At("HEAD"), cancellable = true)
     private void canSupport(BlockEntity be, CallbackInfoReturnable<Boolean> cir) {
-        if (CreateFramedEntityTypes.RED_FROGPORT.is(be))
-            cir.setReturnValue(true);
-        if (CreateFramedEntityTypes.ORANGE_FROGPORT.is(be))
-            cir.setReturnValue(true);
-        if (CreateFramedEntityTypes.YELLOW_FROGPORT.is(be))
-            cir.setReturnValue(true);
-        if (CreateFramedEntityTypes.GREEN_FROGPORT.is(be))
-            cir.setReturnValue(true);
-        if (CreateFramedEntityTypes.BLUE_FROGPORT.is(be))
-            cir.setReturnValue(true);
-        if (CreateFramedEntityTypes.PURPLE_FROGPORT.is(be))
-            cir.setReturnValue(true);
+        for (CreateFramedFrogports frogports : CreateFramedFrogports.values()) {
+            if (frogports.getFrogport().is(be))
+                cir.setReturnValue(frogports.getFrogport().is(be));
+        }
     }
 }
